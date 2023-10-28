412733042332bacd1f45940dd35afb80bd87c84c
MatthewHilliard <matthewhilliard2003@gmail.com>
2023-10-27 20:22:05 -0400

    Imported hamburger menu

--- code/client/src/NavBar.jsx
+++ code/client/src/NavBar.jsx
@@ -1,14 +1,18 @@
 import logo from './assets/forkandknife.png'
+import menu_bar from './assets/menu_bar.png'
 
 function NavBar(){
     return(
-        <div class="flex flex-row">
-            <div class="basis-1/8">
-                <img class='h-16' src={logo}/>
+        <div class="flex flex-row mx-auto my-auto overflow-hidden">
+            <div class="">
+                <img class='h-16 py-2 px-2' src={logo}/>
             </div>
-            <div class="basis-1/8 py-3 text-4xl font-light">
+            <div class="py-3 text-4xl font-light">
                 <h1>Meal Planner</h1>
             </div>
+            <button class=''>
+                <img class='h-16' src={menu_bar}/>
+            </button>
         </div>
     )
 }
