f6ddaa55364a901887580c4d95a592abacef9ec0
MatthewHilliard <matthewhilliard2003@gmail.com>
2023-11-02 19:42:02 -0400

    Imported cors

--- code/server/index.js
+++ code/server/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const app = express();
-
+const cors = require('cors')
 const mongoose = require('mongoose')
 const UserModel = require('./models/Users')
 
+const app = express();
+
 app.use(express.json());
+app.use(cors());
 
 mongoose.connect("mongodb+srv://matthewhilliard:birjit@cluster0.yoaoe5y.mongodb.net/MealPlanner?retryWrites=true&w=majority&appName=AtlasApp")
 
