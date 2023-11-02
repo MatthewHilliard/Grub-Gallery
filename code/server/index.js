032ff16557be276bcd523e26b2112aa13c439a47
MatthewHilliard <matthewhilliard2003@gmail.com>
2023-11-02 19:29:58 -0400

    Got mongo DB working :)

--- code/server/index.js
+++ code/server/index.js
@@ -1,21 +1,20 @@
 const express = require("express");
 const app = express();
+
 const mongoose = require('mongoose')
 const UserModel = require('./models/Users')
 
 mongoose.connect("mongodb+srv://matthewhilliard:birjit@cluster0.yoaoe5y.mongodb.net/MealPlanner?retryWrites=true&w=majority&appName=AtlasApp")
 
-app.get("/getUsers", (req, res) => {
-    UserModel.find({}, (err, result) => {
-        if (err) {
-            res.json(err);
-        }
-        else{
-            res.json(result);
-        }
-    })
+app.get("/getUsers", async (req, res) => {
+    try{
+        const users = await UserModel.find()
+        res.status(200).json(users);
+      } catch (error){
+        res.status(500).json(users)
+      }
 })
 
-app.listen(3000, () => {
+app.listen(5173, () => {
     console.log("SERVER RUNS")
 })
