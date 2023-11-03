78ff52c802ad1a241cf32e242a33e652be75b3bc
MatthewHilliard <matthewhilliard2003@gmail.com>
2023-11-03 12:19:39 -0400

    Added dropdown menu functionality

--- code/client/src/NavBar.jsx
+++ code/client/src/NavBar.jsx
@@ -1,22 +1,30 @@
 import logo from './assets/forkandknife.png'
 import menu_bar from './assets/menu_bar.png'
 import profile_photo from './assets/profile.png'
+import { useState } from 'react'
 
 function NavBar(){
+    const [isOpen, setIsOpen] = useState(false);
     return(
         <div class="flex flex-row mx-auto my-auto overflow-hidden">
             <div class='flex items-center'>
                 <img class='h-16 py-2 px-2' src={logo}/>
                 <h1 class="py-3 text-4xl font-light">Meal Planner</h1>
             </div>
-
             <div class='flex items-center ml-auto mr-5'>
                 <button class=''>
                     <img class='h-10' src={profile_photo}/>
                 </button>
-                <button class='ml-5'> {/* Adjust the ml-2 (margin-left) as needed */}
+                <button onClick={() => setIsOpen((prev) => !prev)} class='ml-5'> {/* Adjust the ml-2 (margin-left) as needed */}
                     <img class='h-12' src={menu_bar}/>
                 </button>
+                {isOpen && <div class="bg-[#B28370] absolute top-14 flex flex-col items-start rounded w-full">
+                    <div class="flex flex-auto flex-col hover:bg-blue-300 cursour-pointer rounded-r-lg border-l-transparent ">
+                        <h3>Favorites</h3>
+                        <h3>Schedule</h3>
+                    </div>
+                </div>
+                    }
             </div>
         </div>
     )
--- code/client/src/main.jsx
+++ code/client/src/main.jsx
@@ -8,7 +8,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <NavBar/>
     <div class="grid grid-cols-2 gap-y-6 relative top-[200px]"> 
       <div class="">
-        <h1 class="text-7xl font-semibold px-10">Find. Plan. Eat</h1>
+        <h1 class="text-7xl font-semibold px-10">Find. Plan. Eat.</h1>
       </div>
       <div class="row-span-3">
         <div class="box-border h-80 w-90 rounded-3xl bg-[#B28370] boxShadow max-w-[600px] ml-auto mr-[20px]">
