9b3e4a4adc435ce9f5a7edc6f811d07630aeefcd
Mensun <mensun@bu.edu>
2023-11-03 12:46:38 -0400

    testing git push

--- code/client/src/main.jsx
+++ code/client/src/main.jsx
@@ -1,30 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import NavBar from './NavBar'
+import NavBar from './components/NavBar'
+import Home from './components/Home'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <NavBar/>
-    <div class="grid grid-cols-2 gap-y-6 relative top-[200px]"> 
-      <div class="">
-        <h1 class="text-7xl font-semibold px-10">Find. Plan. Eat</h1>
-      </div>
-      <div class="row-span-3">
-        <div class="box-border h-80 w-90 rounded-3xl bg-[#B28370] boxShadow max-w-[600px] ml-auto mr-[20px]">
-        </div>
-        <div>
-            <img></img>
-        </div>
-      </div>
-      <div class="px-10">
-        <h2>Discover delectable dishes and seamlessly integrate them into your schedule with Google Calender integration. Elevate your dining experience effortlessly with Meal Planner.</h2>
-      </div>
-      <div class="px-10">
-        <button class="bg-gray-700 hover:bg-gray-900 text-white py-2 px-5 rounded">
-          Browse Meals
-        </button>
-      </div>
-    </div>
+    <NavBar />
+    <Home />
+
   </React.StrictMode>,
 )
