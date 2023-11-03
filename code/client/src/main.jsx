ea6e10bace91a6a722a9094a18a7f7e1b33b9d22
MatthewHilliard <matthewhilliard2003@gmail.com>
2023-11-02 22:18:50 -0400

    Started work on home page

--- code/client/src/main.jsx
+++ code/client/src/main.jsx
@@ -6,5 +6,21 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <NavBar/>
+    <div class="grid grid-cols-2 gap-y-6"> 
+      <div class="">
+        <h1 class="text-7xl font-semibold px-10">Find. Plan. Eat</h1>
+      </div>
+      <div class="row-span-2">
+        <h1>Images go here</h1>
+      </div>
+      <div class="px-10">
+        <h2>Discover delectable dishes and seamlessly integrate them into your schedule with Google Calender integration. Elevate your dining experience effortlessly with Meal Planner.</h2>
+      </div>
+      <div class="px-10">
+        <button class="bg-gray-700 hover:bg-gray-900 text-white py-2 px-5 rounded">
+          Browse Meals
+        </button>
+      </div>
+    </div>
   </React.StrictMode>,
 )
