e816fba3da6bd11f1aa475f09737497954766025
bvogler10 <brinjavogler@gmail.com>
2023-11-04 14:17:48 -0400

    actual initial google oauth stuff for login page but it doesn't work yet

--- code/client/src/App.jsx
+++ code/client/src/App.jsx
@@ -7,7 +7,7 @@ import './index.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
-
+  
   return (
     <Router>
         <NavBar/>
--- code/client/src/components/Login.jsx
+++ code/client/src/components/Login.jsx
@@ -1,8 +1,28 @@
+import { useEffect } from 'react';
+
 function Login() {
 
+    function handleCallbackResponse(response) {
+        console.log("Encoded JWT ID Token: " + response.credential);
+    }
+
+    useEffect(() => {
+        /* global google */
+        google.accounts.id.initialize({
+          client_id: " 1040079796712-hqafp6aje8gvaa6iotq38i478h5n8f83.apps.googleusercontent.com",
+          callback: handleCallbackResponse
+        });
+    
+        google.accounts.id.renderButton(
+          document.getElementById("signInDiv"),
+          { theme: "outline", size: "large"}
+        )
+    }, []);
+  
     return (
         <div>
             this is a page to register users
+            <div id="signInDiv"></div>
         </div>
     )
 }
