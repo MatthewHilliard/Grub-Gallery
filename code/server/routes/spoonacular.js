bfcfd5501828c1d1bd4bd14e05cc311483c1cb46
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2023-11-04 23:30:04 -0400

    added support for Spoonacular api call in backend and connected to frontend (check website console after entering keyword in searchbar for demo)

--- code/server/routes/spoonacular.js
+++ code/server/routes/spoonacular.js
@@ -1,20 +1,39 @@
 // Note: this file exists to process spoonacular api call from frontend
 const express = require("express")
 const router = express.Router()
+const axios = require('axios');
 const cors = require('cors')
-const port = 3000
 
 
 router.use(express.json())
 router.use(cors())
 
-router.get("/meals", (req, res) => {
+router.post("/meals", async (req, res) => {
   try {
-    const meals = ["pasta", "pasta", "pasta"]
+    // initialization for spoonacular api call
+    const param = req.body.searchString
+    let config = {
+      method: 'get',
+      url: `https://api.spoonacular.com/recipes/complexSearch?query=${param}&apiKey=ea7e76522981472aaa961dde6913a48e`,
+      headers: { }
+    }
 
-    res.status(200).json(meals)
+    // call spoonacular api
+    try {
+      const response = await axios.request(config)
+      meals = JSON.stringify(response.data)
+      console.log(meals)
+      res.status(200).json(meals)
+    }
+    catch (error) {
+      console.log("Error calling Spoonacular API:", error)
+      res.status(500).json({ message: 'Error calling Spoonacular API' })
+    }
+
+
   } catch (error) {
-    console.log(error)
+    console.log("Error in post to Spoonacular:", error)
+    res.status(500).json({ message: 'Error in post to Spoonacular' })
   }
 })
 
