99cc2e8f4901c8053e11efab0ccf58593555b8a9
bvogler10 <brinjavogler@gmail.com>
2023-11-05 13:59:45 -0500

    sign in and sign out buttons, pfp and name shown when logged in, google prompt to log in (but everything is ugly sorry)

--- code/client/src/components/Login.jsx
+++ code/client/src/components/Login.jsx
@@ -1,12 +1,22 @@
 import { jwtDecode } from 'jwt-decode';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function Login() {
-    
+    const [ user, setUser ] = useState({});
+
     function handleCallbackResponse(response) {
         console.log("Encoded JWT ID Token: " + response.credential);
         var userObject = jwtDecode(response.credential);
         console.log(userObject);
+        setUser(userObject);
+        //hides sign in button when user is logged in
+        document.getElementById("signInDiv").hidden = true;
+    }
+    
+    function handleSignOut(event) {
+        setUser({});
+        //restores the sign in button when user logs out
+        document.getElementById("signInDiv").hidden = false;
     }
 
     useEffect(() => {
@@ -20,12 +30,28 @@ function Login() {
           document.getElementById("signInDiv"),
           { theme: "outline", size: "large"}
         )
+        //prompts user and asks if they want to log in with previously used accounts
+        google.accounts.id.prompt();
     }, []);
+
+    //If we have no user: sign in button
+    //If we have a user: show the log out button
 
     return (
         <div>
             this is a page to register users
             <div id="signInDiv" className="w-64"></div>
+            {/* If the user is not empty, show sign out button*/}
+            { Object.keys(user).length != 0 &&
+                <button onClick={ (e) => handleSignOut(e)}>Sign Out</button>
+            }
+            {/* shows name and pfp of google user */}
+            {   user && 
+                <div> 
+                    <img src={user.picture}></img>
+                    <h3>{user.name}</h3>
+                </div>
+            }   
         </div>
     )
 }
