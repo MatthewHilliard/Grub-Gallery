0e6d183d4daf5542f208f627a79d342ad39bdb70
MatthewHilliard <matthewhilliard2003@gmail.com>
2023-11-06 17:30:06 -0500

    Imported images and further styled home page

--- code/client/src/components/Home.jsx
+++ code/client/src/components/Home.jsx
@@ -1,19 +1,29 @@
+import food1 from "../assets/spaghetti.jpg"
+import food2 from "../assets/food2.jpg"
+import food3 from "../assets/food3.jpg"
+import food4 from "../assets/food4.jpg"
+import food5 from "../assets/food5.jpg"
+import food6 from "../assets/food6.jpg"
+
 function Home() {
   
     return(
       <div className="grid grid-cols-2 gap-y-6 relative top-[200px]"> 
-      
-        <h1 className="text-7xl font-semibold px-10">Find. Plan. Eat.</h1>
-
-        <div className="row-span-3">
+        <h1 className="text-8xl font-semibold px-10">Find. Plan. Eat.</h1>
+        <div className="row-span-4">
           <div className="box-border h-[500px] w-[800px] rounded-3xl bg-[#B28370] boxShadow ml-auto mr-[20px]">
-          </div>
-          <div className="grid col-span-3">
-            <img></img>
+            <div className="grid grid-cols-3 p-9 gap-12">
+              <img src={food1} className ="h-48 rounded-xl"/>
+              <img src={food2} className ="h-48 rounded-xl"/>
+              <img src={food3} className ="h-48 rounded-xl"/>
+              <img src={food4} className ="h-48 rounded-xl"/>
+              <img src={food5} className ="h-48 rounded-xl"/>
+              <img src={food6} className ="h-48 rounded-xl"/>
+            </div>
           </div>
         </div>
 
-        <div className="px-10">
+        <div className="px-10 text-lg py-6">
           <h2>Discover delectable dishes and seamlessly integrate them into your schedule with Google Calender integration. Elevate your dining experience effortlessly with Meal Planner.</h2>
         </div>
 
