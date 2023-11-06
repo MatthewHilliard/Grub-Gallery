d6353a8f9d272775563255140cc978ff62496c7f
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2023-11-05 23:28:26 -0500

    minor change - click on 'Meal Planner' title to route to Home.jsx

--- code/client/src/components/NavBar.jsx
+++ code/client/src/components/NavBar.jsx
@@ -55,10 +55,12 @@ function NavBar({ setSearchMealsList }) {
 
     return(
         <div className="flex flex-row mx-auto my-auto overflow-hidden">
-            <div className='flex items-center flex-shrink-0'>
-                <Link to="/"><img className='h-16 py-2 px-2' src={logo}/></Link>
-                <h1 className="py-3 text-4xl font-light">Meal Planner</h1>
-            </div>
+            <Link to="/">
+                <div className='flex items-center flex-shrink-0'>
+                    <img className='h-16 py-2 px-2' src={logo}/>
+                    <h1 className="py-3 text-4xl font-light">Meal Planner</h1>
+                </div>
+            </Link>
 
             <div className="ml-5 mr-5 mt-2 w-[350px]"> {/*Can change the placement as needed*/}
                     <input 
