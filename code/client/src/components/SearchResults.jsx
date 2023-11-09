9d6e529d9e4e98a67352d2e9684beca5a3b6f6a1
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2023-11-08 21:16:35 -0500

    respond to Mensun comments

--- code/client/src/components/SearchResults.jsx
+++ code/client/src/components/SearchResults.jsx
@@ -1,7 +1,10 @@
 function SearchResults(props) {
     {/* note: can also use "props" to represent every input taken for these functions. So props.searchMealsList is the same thing */ }
+        // "props" is just a standard way of passing parameters from a high level to lower level component - De Rocco
     {/* why does it print props.searchMealsList twice in test console? */ }
+        // that's just how react works lol ig they wanna make sure we don't miss anything :// lol - De Rocco
     { /* Uses the map function on the searchMealsList and does some "work" using the element and an index which starts from 0 */ }
+        // Yuh
     const mealsList = props.searchMealsList.map((element, index) => (
 
         // LOOKS UNDER CONSTRUCTION
