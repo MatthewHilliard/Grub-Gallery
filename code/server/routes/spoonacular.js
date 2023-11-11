4de6a52e1a8aa87bd1966ba5574b52e902975205
Fung-Sean <fsean100@gmail.com>
2023-11-10 23:41:43 -0500

    Adding functionality to the recipes pages

--- code/server/routes/spoonacular.js
+++ code/server/routes/spoonacular.js
@@ -51,7 +51,7 @@ router.post("/browse", async (req, res) => {
     const param = req.body.searchString // unused input from frontend?
     let config = {
       method: 'get',
-      url: `https://api.spoonacular.com/recipes/random?number=${numberOfRecipes}&apiKey=2ad32db11bc142a9ab36a432b8037889`,
+      url: `https://api.spoonacular.com/recipes/random?number=${numberOfRecipes}&apiKey=af7e54fc940247eea38d894e383d7fa9`,
       headers: { }
     }
 
@@ -83,7 +83,7 @@ router.post("/recipe", async (req, res) => {
     // initialization for spoonacular api call
     let config = {
       method: 'get',
-      url: `https://api.spoonacular.com/recipes/${id}/information?apiKey=2ad32db11bc142a9ab36a432b8037889`,
+      url: `https://api.spoonacular.com/recipes/${id}/information?apiKey=af7e54fc940247eea38d894e383d7fa9`,
       headers: { }
     }
 
