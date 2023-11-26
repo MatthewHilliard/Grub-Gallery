d1819f6af0656c26bbfc510b188c106473eed321
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2023-11-26 15:46:34 -0500

    minor styling changes (swapped favorite icons)

--- code/client/src/components/DisplayResults.jsx
+++ code/client/src/components/DisplayResults.jsx
@@ -87,15 +87,15 @@ const Card = styled.div`
 
   .favoriteIcon {
     position: absolute;
-    margin-top: -20px;
-    margin-left: -20px;
-    width: 40px;
+    margin-top: -10px;
+    margin-left: -10px;
+    width: 30px;
     z-index: 1; /* Ensure the icon is on top */
 
-    backdrop-filter: blur(4px);
-  // box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
-  // background: linear-gradient(to bottom, rgba(255, 255, 255, 0), rgba(255, 255, 255, 1));
-  border-radius: 50%; /* Optional: to create a circular background */
+    // backdrop-filter: blur(10px);
+    // box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
+    // background: linear-gradient(to bottom, rgba(255, 255, 255, 0), rgba(255, 255, 255, 1));
+    // border-radius: 50%; /* Optional: to create a circular background */
 
 
     &:hover {
