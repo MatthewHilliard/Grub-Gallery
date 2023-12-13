a8d8d76fd8c604ddfcdaf50f2b2b88cd433fda60
Mensun <mensun@bu.edu>
2023-12-12 19:27:02 -0500

    some moar stoopid bugz

--- code/client/src/components/Login.jsx
+++ code/client/src/components/Login.jsx
@@ -11,31 +11,31 @@ function Login({ isAuthenticated }) {
     const handleSignIn = async () => {
         try {
             // if google auth fails (jump to error catch)
-            const { userResult } = await signInWithGoogle();
-            
-            if (userResult) {
+            const { result } = await signInWithGoogle();
+            if (result) {
                 // body: object of data being sent to backend endpoint
                 const body = {
-                    name: userResult.displayName,
-                    email: userResult.email,
-                    user_id: userResult.uid
+                    name: result.displayName,
+                    email: result.email,
+                    user_id: result.uid
                 };
-                
+
+
                 // Call backend's MongoDB 'createUsers' endpoint to create the user
                 Axios.post("http://localhost:3000/users/createUser", body)
-                .then((response) => {
-                    console.log("Create User API call response: ", response);
-                })
-                .catch((error) => {
-                    console.log("Error making Axios post request:", error);
-                });
+                    .then((response) => {
+                        console.log("Create User API call response: ", response);
+                    })
+                    .catch((error) => {
+                        console.log("Error making Axios post request:", error);
+                    });
             }
-            
+
         } catch (error) {
             console.log("Error handling sign in:", error);
         }
     };
-    
+
 
     return (
         <div className="flex justify-center items-center gap-[200px] mt-[100px] ml-[100px] mr-[100px]">
@@ -46,7 +46,7 @@ function Login({ isAuthenticated }) {
 
                 {/* Main Content */}
                 <h1 className="text-[34px] font-semibold">Find healthy and trusted recipes</h1>
-                
+
                 {/* Checkmarks and Text */}
                 <div className="flex flex-col gap-[20px] text-[24px]">
                     <div className="flex items-center mb-4">
@@ -67,14 +67,14 @@ function Login({ isAuthenticated }) {
                         </h1>
                     </div>
                 </div>
-                
+
             </div>
 
 
             {/* Column 2 */}
             <div className="flex justify-center items-center box-border h-[500px] w-[600px] min-w-[400px] rounded-3xl bg-[#B28370] boxShadow">
                 {/* Sign Out Button */}
-                {isAuthenticated ? 
+                {isAuthenticated ?
                     <button className="google-btn w-60 h-10" onClick={handleSignOut}>
                         Sign Out
                     </button>
@@ -84,7 +84,7 @@ function Login({ isAuthenticated }) {
                             <h1 className="text-[40px] font-semibold">Sign up or log in</h1>
                             <p className="text-[20px]">Login below to start utilizing GrubGallery’s advanced features (e.g. Google Calendar, personalized meal suggestions...)</p>
                         </div>
-                        
+
                         <button className="google-btn mb-4 md:mb-0 ml-auto mr-auto" onClick={handleSignIn}>
                             Sign In With Google
                         </button>
